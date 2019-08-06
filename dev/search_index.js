c6263bc580de5f4065078ec6e4f17647367e4956
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-08-06 12:54:52 +0000

    build based on efae2e3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#BinaryClassifierEvaluation.jl-Documentation-1","page":"Home","title":"BinaryClassifierEvaluation.jl Documentation","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"This package implements an effective and parallelizable algorithm for evaluating binary classifiers. It computes the confusion matrix of the classifier and all the measures stemming from it.","category":"page"}]
+}
